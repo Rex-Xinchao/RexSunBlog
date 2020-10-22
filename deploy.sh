7624d8d8b47e2ea236bf18b9fc0e42e21e0643c7
Rex-Xinchao <13601935905@163.com>
2020-10-22 15:06:50 +0800

    update sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env sh
 set -e
 cd docs/.vuepress/dist
-git init
-git config --global user.email "13601935905@163.com"
-git config --global user.name "Rex-Xinchao"
-git add -A
-git commit -m 'deploy'
 git push -f https://github.com/Rex-Xinchao/Rex-Xinchao.github.io.git master
